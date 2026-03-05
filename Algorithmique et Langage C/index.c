19ae28808c88ead956376a45158baea48d870678
ibrahima-4 <xafjunior04@gmail.com>
2026-03-05 22:03:34 +0000

    .

---
+++ Algorithmique et Langage C/index.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+        int main(){
+            int a;
+            printf("Saisir une valeur : ");
+            scanf("%d",&a);
+            printf("Vous avez saisi : %d",a);
+            return 0;
+        }
