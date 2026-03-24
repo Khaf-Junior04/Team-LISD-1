b4dd2f49649de511cb55214699b65405f3567c4c
ibrahima-4 <xafjunior04@gmail.com>
2026-03-24 23:42:22 +0100

    cours_du_24_03_2026

--- Algorithmique et Langage C/bouclesPour.c
+++ Algorithmique et Langage C/bouclesPour.c
@@ -3,9 +3,20 @@
     int main(){
         int compteur;
 
-        // for - while - do ... while 
-        // La boucle for 
-        for(compteur =  1;compteur <= 10;compteur = compteur + 1){
-            printf("%d \n",compteur);
-        }
+        // for(compteur = 0;compteur <= 10;compteur = compteur + 1){
+        //     printf("%d \n", compteur);
+        // }  
+        // compteur = 0;
+        // while(compteur <= 10){
+        //     printf("%d \n ",compteur);
+        //     compteur = compteur + 1;
+        // }
+
+        compteur = 0;
+        do{
+            printf("Bonsoir Aminata et Khadija ! \n");
+            compteur = compteur + 1;
+        }while(compteur <= 10);
+
+    return 0; 
     }
